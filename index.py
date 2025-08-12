b505734e6cb97bc280d04830616da7e8eb9a1c0f
Gustavo Henrique <gustavohenri970@gmail.com>
2025-08-12 16:45:24 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+
+
+
+# * MULTIPLICAÇÃO
+n1 = int(input('digite um numero: '))
+n2 = int(input('digite outro numero para multiplicar ao primeiro: '))
+
+print(f"A multiplicação de {n1} e {n2} é igual a {n1 * n2}")
+
+
+
+# / DIVISÃO
+nu1 = float(input('digite um numero: '))
+nu2 = float(input('digite outro numero para dividir ao primeiro: '))
+
+print(f"A divisão de {nu1} e {nu2} é igual a {nu1 / nu2}")
+
+
+
+
+# // DIVISÃO INTEIRA
+num1 = int(input('digite um numero: '))
+num2 = int(input('digite outro numero para dividir ao primeiro: '))
+
+print(f"A divisão de {num1} e {num2} é igual a {num1 // num2}")
+
+
+# % (RESTO DA DIVISÃO)
+
+nume1 = int(input('digite um numero: '))
+nume2 = int(input('digite outro numero'))
+
+print(f" o resto da divisão é {nume1 % nume2} ")
+
+
+# ** EXPONENCIAÇÃO
+
+
+nume1 = int(input('digite um numero: '))
+nume2 = int(input('digite outro numero'))
+
+print(f" o resto da divisão é {nume1 ** nume2} ")
+
+
+
+
